52fb87a8fb56dad432e18c3c9a405d11fe6f9a10
lyc <601753574@qq.com>
2018-06-12 13:29:39 +0800

    new

--- mytest/wordstovec.py
+++ mytest/wordstovec.py
@@ -22,17 +22,39 @@ import logging
 # print("-------------------------------\n")
 
 
-# 用 word2vec 进行训练
-sentences=word2vec.Text8Corpus('软件_2018-05-01_2018-05-04')
-# 第一个参数是训练语料，第二个参数是小于该数的单词会被剔除，默认值为5, 第三个参数是神经网络的隐藏层单元数，默认为100
-model=word2vec.Word2Vec(sentences,min_count=1, size=200)
-print(model["再说"])
-# y2=model.similarity(u"不错", u"好吃") #计算两个词之间的余弦距离
-# print(y2)
-model.wv.save_word2vec_format("软件_2018-05-01_2018-05-04_model.bin",binary=True)
-model= gensim.models.KeyedVectors.load_word2vec_format("软件_2018-05-01_2018-05-04_model.bin", binary=True)
-for i in model.most_similar(u"再说"): #计算余弦距离最接近“滋润”的10个词
+
+
+def cut_txt(old_file):
+    cut_file = old_file.split('.')[0] + '_cut.txt'
+
+    try:
+        fi = open(old_file, 'r', encoding='utf-8')
+    except BaseException as e:  # 因BaseException是所有错误的基类，用它可以获得所有错误类型
+        print(Exception, ":", e)    # 追踪错误详细信息
+
+    text = fi.read()  # 获取文本内容
+    new_text = jieba.cut(text, cut_all=False)  # 精确模式
+    print(new_text)     #一个生成器
+    str_out = ' '.join(new_text).replace('，', '').replace('。', '').replace('？', '').replace('！', '') \
+        .replace('“', '').replace('”', '').replace('：', '').replace('…', '').replace('（', '').replace('）', '') \
+        .replace('—', '').replace('《', '').replace('》', '').replace('、', '').replace('‘', '') \
+        .replace('’', '')     # 去掉标点符号
+    fo = open(cut_file, 'w', encoding='utf-8')
+    fo.write(str_out)
+    return cut_file
+
+def model_train(train_file_name, save_model_file):  # model_file_name为训练语料的路径,save_model为保存模型名
+    import logging
+    # 模型训练，生成词向量
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    sentences = word2vec.Text8Corpus(train_file_name)  # 加载语料
+    model = word2vec.Word2Vec(sentences, min_count=2,size=300)  # 训练skip-gram模型; 默认window=5
+    model.wv.save_word2vec_format(save_model_file + ".bin", binary=True)   # 以二进制类型保存模型以便重用
+    return model,str(save_model_file + ".bin")
+
+outfile = cut_txt('all.txt')
+model0,outmodel = model_train(outfile, 'jinyong')
+model = gensim.models.KeyedVectors.load_word2vec_format(outmodel, binary=True)
+
+for i in model.most_similar(u"清朝"): #计算余弦距离最接近“滋润”的10个词
     print(i[0],i[1])
-import os
-if not os.path.exists('path'):
-    os.makedirs('path')
